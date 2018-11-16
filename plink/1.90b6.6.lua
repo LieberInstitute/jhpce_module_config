5d81c7e8627cda496aa59680b875f3f479f4d873
lcolladotor <lcollado@jhsph.edu>
2018-11-16 15:46:29 -0500

    add plink 1.90b6.6

---
+++ plink/1.90b6.6.lua
@@ -0,0 +1,16 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads plink version 1.90b6.6.
+]])
+
+whatis("plink")
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for plink/1.90b6.6")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/plink/1.90b6.6")
