6d6a5fb166c4418f036b4b1404972734751bda1b
lcolladotor <lcollado@jhsph.edu>
2019-08-29 11:49:49 -0400

    add git-status-size from github

---
+++ git-status-size/github.lua
@@ -0,0 +1,17 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads git-status-size version github.
+]])
+
+whatis("git-status-size")
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for git-status-size/github")
+load('git')
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/git-status-size/github/git-status-size")
