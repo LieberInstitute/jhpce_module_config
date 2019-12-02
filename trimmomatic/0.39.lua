a5fa5586653e07b1717a77bd97e5ce9f80a5731a
Nick-Eagles <nick.eagles@libd.org>
2019-12-02 17:09:33 -0500

    Add the lua file for trimmomatic 0.39, which was just added to the module source

---
+++ trimmomatic/0.39.lua
@@ -0,0 +1,17 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads Trimmomatic version 0.39.
+]])
+
+whatis("Trimmomatic")
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for Trimmomatic/0.39")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/Trimmomatic/0.39/Trimmomatic-0.39")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/Trimmomatic/0.39/Trimmomatic-0.39/adapters")
