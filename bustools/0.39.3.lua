ab0004690315d3ed0aeb33f3a5d728711361e9a7
lcolladotor <lcollado@jhsph.edu>
2019-12-06 14:29:29 -0500

    add bustools 0.39.3

---
+++ bustools/0.39.3.lua
@@ -0,0 +1,16 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads bustools version 0.39.3.
+]])
+
+whatis("bustools")
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for bustools/0.39.3")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/bustools/0.39.3/bustools")
