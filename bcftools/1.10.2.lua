76a168177d8986e857918dee9b5a5fb2a21237d8
BriannaBarry <bbarry3@jhmi.edu>
2020-03-10 19:24:45 -0400

    Added bcftools-1.10.2 and updated permissions for bcftools-1.10.2 and samtools-1.10.

---
+++ bcftools/1.10.2.lua
@@ -0,0 +1,18 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads BCFTools version 1.10.2.
+]])
+
+whatis("BCFTools")
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for BCFTools/1.10.2")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/bcftools/1.10.2/bin")
+prepend_path("MANPATH","/jhpce/shared/jhpce/libd/bcftools/1.10.2/share/man/man1")
+
