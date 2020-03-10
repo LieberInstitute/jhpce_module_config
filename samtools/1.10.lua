83005f71eb978bc9c48bf5ea6ab64d89adbea53d
BriannaBarry <bbarry3@jhmi.edu>
2020-03-10 19:08:07 -0400

    Added samtools-1.10

---
+++ samtools/1.10.lua
@@ -0,0 +1,19 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads samtools version 1.10.
+]])
+
+whatis("samtools")
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for samtools/1.10")
+-- could be necessary (it's used on the core modules for samtools 1.0 and 1.1 but not on 1.6)
+-- prereq("gcc/4.4.7")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/samtools/1.10/bin")
+prepend_path("MANPATH", "/jhpce/shared/jhpce/libd/samtools/1.10/share/man/man1")
