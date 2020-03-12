e4d53f3f1df6fa7d22417259195fb8cae518dc06
Nick-Eagles <nick.eagles@libd.org>
2020-03-12 15:23:15 -0400

    Add lua for LDSC

---
+++ ldsc/1.0.1.lua
@@ -0,0 +1,16 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads LDSC version 1.0.1.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for LDSC/1.0.1")
+load('python/3.7.3')
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/ldsc/1.0.1/ldsc")
+setenv('OPENBLAS_NUM_THREADS', 1)
