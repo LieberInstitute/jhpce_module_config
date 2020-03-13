04c0ebedae00da2d627b2659f358a8239b15bbc0
BriannaBarry <bbarry3@jhmi.edu>
2020-03-13 19:16:46 -0400

    Fixed broken rseqc-2.6.5 module by adding dependencies and reinstalling.

--- rseqc/2.6.5.lua
+++ rseqc/2.6.5.lua
@@ -13,6 +13,6 @@ This package can only be loaded on a compute or transfer node. Please use qrsh t
 end
 
 LmodMessage("Loading LIBD module for RSeQC/2.6.5")
-load('python/2.7.9')
-prepend_path("PYTHONPATH", "/jhpce/shared/jhpce/libd/rseqc/2.6.5/RSeQC-2.6.5/bin/jhpce/shared/community/compiler/gcc/4.4.7/python/2.7.9/lib/python2.7/site-packages")
-prepend_path("PATH", "/jhpce/shared/jhpce/libd/rseqc/2.6.5/RSeQC-2.6.5/bin/jhpce/shared/community/compiler/gcc/4.4.7/python/2.7.9/bin")
+load('python/2.7')
+prepend_path("PYTHONPATH", "/jhpce/shared/jhpce/libd/rseqc/2.6.5/RSeQC-2.6.5/bin//jhpce/shared/jhpce/core/python/2.7.9/lib/python2.7/site-packages")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/rseqc/2.6.5/RSeQC-2.6.5/bin//jhpce/shared/jhpce/core/python/2.7.9/bin/")
