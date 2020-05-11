632a53093deaf1e5b5cdbab3ce96d542a44e2e1f
joshstolz <josh.stolz@libd.org>
2020-05-11 16:18:57 -0400

    update PRSice path and version name

--- PRSice/2.2.8.lua
+++ PRSice/2.2.8.lua
@@ -3,15 +3,15 @@
 -- vim:ft=lua:et:ts=4
 
 help([[
-This module loads PRSice-2 version 2.2.13.
+This module loads PRSice version 2.2.8.
 ]])
 
-whatis("PRSice-2")
+whatis("PRSice")
 
 if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
     LmodError("\
 This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
 end
 
-LmodMessage("Loading LIBD module for PRSice-2/v2.2.13")
-prepend_path("PATH", "/jhpce/shared/jhpce/libd/PRSice-2/v2.2.13")
+LmodMessage("Loading LIBD module for PRSice/2.2.8")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/PRSice/2.2.8/PRSice-local/")
