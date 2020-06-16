1efb5e9334d4588afe774b8840f260316e838e58
Nick-Eagles <nick.eagles@libd.org>
2020-06-16 14:41:03 -0400

    Add lua file for HipSTR 0.7

---
+++ hipstr/0.7.lua
@@ -0,0 +1,14 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads HipSTR version 0.7.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for HipSTR/0.7")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/hipstr/0.7/HipSTR")
