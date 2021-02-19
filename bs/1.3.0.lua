68be2e8f8cdbad77a3e2078b928060f429e0bee9
lcolladotor <lcollado@jhsph.edu>
2021-02-19 11:16:45 -0500

    Added a lua file for bs 1.3.0

---
+++ bs/1.3.0.lua
@@ -0,0 +1,16 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads bs version 1.3.0.
+]])
+
+whatis("bs")
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for bs/1.3.0")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/bs/1.3.0/bin")
