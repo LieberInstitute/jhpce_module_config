16af1d322a0b76e4ed1835e0a1115a3042d6b4df
Nick-Eagles <nick.eagles@libd.org>
2021-04-15 16:09:53 -0400

    Add lua for samblaster 0.1.26

---
+++ samblaster/0.1.26.lua
@@ -0,0 +1,14 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads samblaster v0.1.26.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for samblaster/0.1.26")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/samblaster/0.1.26/samblaster-v.0.1.26")
