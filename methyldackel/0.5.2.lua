9396a9b9ada42c9db2122d245cc0e7f907bf3ffd
Nick-Eagles <nick.eagles@libd.org>
2021-04-15 15:37:30 -0400

    Add lua for methyldackel 0.5.2

---
+++ methyldackel/0.5.2.lua
@@ -0,0 +1,16 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads MethylDackel 0.5.2.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for methyldackel/0.5.2")
+LmodMessage("Note that the software is invoked as 'MethylDackel', not 'methyldackel'.")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/methyldackel/0.5.2")
+prepend_path("LD_LIBRARY_PATH", "/jhpce/shared/jhpce/libd/methyldackel/0.5.2/lib")
