63ea375402591b64a1ddb8144cb9c4f259f8dbd4
Nick-Eagles <nick.eagles@libd.org>
2021-04-30 15:44:38 -0400

    Load python 3.7.3 with hisat2/2.2.1

--- hisat2/2.2.1.lua
+++ hisat2/2.2.1.lua
@@ -2,7 +2,7 @@
 -- vim:ft=lua:et:ts=4
 
 help([[
-This module loads HISAT2 version 2.2.1.
+This module loads HISAT2 version 2.2.1 and python 3.7.3.
 ]])
 
 whatis("HISAT2")
@@ -11,6 +11,8 @@ if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.ge
     LmodError("\
 This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
 end
+
+load('python/3.7.3')
 
 LmodMessage("Loading LIBD module for HISAT2/2.2.1")
 prepend_path("PATH", "/jhpce/shared/jhpce/libd/hisat2/2.2.1/hisat2-2.2.1")
