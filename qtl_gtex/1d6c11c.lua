3e8358bc534284db96768688fae96af0291ea925
aseyedia <arta.seyedian@gmail.com>
2021-05-19 11:18:51 -0400

    added gtex-pipeline's qtl scripts

---
+++ qtl_gtex/1d6c11c.lua
@@ -0,0 +1,17 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads GTEx pipeline QTL scripts.
+]])
+
+whatis("qtl_gtex")
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for qtl_gtex")
+load("python/3.7.3")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/gtex-pipeline/qtl/src")
