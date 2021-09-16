06a0d67dd02d590157d4fb3411d82d17ab2e0ca4
Nick-Eagles <nick.eagles@libd.org>
2021-09-16 16:24:13 -0400

    Remove the requirement that users need to set their working directory to properly use the RESEPT module

--- resept/1.0.0.lua
+++ resept/1.0.0.lua
@@ -8,8 +8,11 @@ This package can only be loaded on a compute or transfer node. Please use qrsh t
 end
 
 LmodMessage("Loading LIBD module for resept/1.0.0")
-LmodMessage("Note: python scripts must be run from the following working directory:")
-LmodMessage("    /jhpce/shared/jhpce/libd/resept/1.0.0/RESEPT")
+LmodMessage("Note: python scripts should be invoked using 'which':")
+LmodMessage("    'python $(which RGB_images_pipeline.py)', rather than:")
+LmodMessage("    'python RGB_images_pipeline.py'.")
 
 load('python/3.7.3')
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/resept/1.0.0/RESEPT/scGNNsp_space")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/resept/1.0.0/RESEPT")
 prepend_path("PATH", "/jhpce/shared/jhpce/libd/resept/1.0.0/resept_venv/bin")
