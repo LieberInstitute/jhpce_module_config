0e299eac90e4d7c34772f04f85ce00b0469a5ba8
Nick-Eagles <nick.eagles@libd.org>
2021-09-22 15:34:22 -0400

    Add lua for SpaGCN 1.2.0

---
+++ spagcn/1.2.0.lua
@@ -0,0 +1,13 @@
+help([[
+This module loads SpaGCN 1.2.0 for python 3.7.3.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for spagcn/1.2.0")
+
+load('python/3.7.3')
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/spagcn/1.2.0/spagcn_venv/bin")
