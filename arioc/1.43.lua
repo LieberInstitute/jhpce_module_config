708976061dfa2f993ac7a84bd58254beb56d1bc2
Nick-Eagles <nick.eagles@libd.org>
2021-11-24 15:06:40 -0500

    Remove unnecessary environment variable modifications performed when loading arioc (actually, the paths previously used no longer even exist)

--- arioc/1.43.lua
+++ arioc/1.43.lua
@@ -13,7 +13,4 @@ end
 LmodMessage("Loading LIBD module for Arioc/1.43")
 
 load("gcc/5.5.0")
-prepend_path("CPATH", "/usr/local/cuda-10.1/targets/x86_64-linux/include")
-prepend_path("LD_LIBRARY_PATH", "/usr/local/cuda-10.1/targets/x86_64-linux/lib")
-prepend_path("PATH", "/usr/local/cuda-10.1/bin")
 prepend_path("PATH", "/jhpce/shared/jhpce/libd/arioc/1.43/bin")
