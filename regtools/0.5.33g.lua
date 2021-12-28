eef09b19f55dd4fbdeb8c846b0906033df4dfe21
Nick-Eagles <nick.eagles@libd.org>
2021-12-28 11:51:15 -0500

    Add lua for gpertea's fork of regtools (version 0.5.33g)

---
+++ regtools/0.5.33g.lua
@@ -0,0 +1,16 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads gpertea's fork of regtools: version 0.5.33g.
+]])
+
+whatis("regtools")
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for regtools/0.5.33g")
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/regtools/0.5.33g/regtools-0.5.33g/build")
