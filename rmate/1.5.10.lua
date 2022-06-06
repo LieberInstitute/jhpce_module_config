c11f0bbe7787089e754e60df741845c6cfc42b29
lcolladotor <lcollado@jhsph.edu>
2022-06-06 16:30:51 -0400

    Add rmate 1.5.10

---
+++ rmate/1.5.10.lua
@@ -0,0 +1,17 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads rmate version 1.5.10.
+]])
+
+whatis("rmate")
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for rmate/1.5.10")
+load('ruby/3.1.2')
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/rmate/1.5.10/rmate/bin")
