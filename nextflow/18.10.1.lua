07fca58248e79db73e6993f617bfee1c5ce57a07
Nick-Eagles <nick.eagles@libd.org>
2022-07-06 16:51:27 -0400

    Make sure conda_R is unloaded when using nextflow modules, to avoid a difficult-to-diagnose error when running the nextflow binary for the first time with conda_R loaded and _JAVA_OPTIONS set

--- nextflow/18.10.1.lua
+++ nextflow/18.10.1.lua
@@ -12,6 +12,16 @@ if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.ge
 This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
 end
 
-LmodMessage("Loading LIBD module for nextflow/18.10.1")
-LmodMessage('## To deal with running nextflow without requesting much more memory check https://jhpce.jhu.edu/question/why-do-i-get-memory-errors-when-running-java/. You might want to use something like: export _JAVA_OPTIONS="-Xms5g -Xmx6g"')
+if (mode() == "load") then
+    LmodMessage("Loading LIBD module for nextflow/18.10.1")
+    LmodMessage('## To deal with running nextflow without requesting much more memory check https://jhpce.jhu.edu/question/why-do-i-get-memory-errors-when-running-java/. You might want to use something like: export _JAVA_OPTIONS="-Xms5g -Xmx6g"')
+
+    -- The 'conda_R' module seems to interfere with first-time execution of
+    -- nextflow when '_JAVA_OPTIONS' is set. To avoid this confusing scenario,
+    -- make sure 'conda_R' is not loaded while using nextflow
+    unload('conda_R')
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD module for nextflow/18.10.1")
+end
+
 prepend_path("PATH", "/jhpce/shared/jhpce/libd/nextflow/18.10.1")
