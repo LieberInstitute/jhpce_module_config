23bba3c16496d9c5f3b3f416a6e0779c43b980c9
Nick-Eagles <nick.eagles@libd.org>
2022-09-20 12:08:09 -0400

    Add lua for cellranger-arc 2.0.2

---
+++ cellranger_arc/2.0.2.lua
@@ -0,0 +1,20 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads cellranger-arc 2.0.2.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD module for cellranger_arc/2.0.2")
+    LmodMessage("Reference files, for use with the '--transcriptome' argument, can be accessed or downloaded into /dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x.")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD module for cellranger_arc/2.0.2")
+end
+
+prepend_path("PATH", "/jhpce/shared/jhpce/libd/cellranger_arc/2.0.2/cellranger-arc-2.0.2")
