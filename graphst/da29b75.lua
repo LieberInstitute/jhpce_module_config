c7bcb64afa2b52a508e60ffebd74eeaef3049708
Nick-Eagles <nick.eagles@libd.org>
2023-02-27 15:05:17 -0500

    Add lua for GraphST

---
+++ graphst/da29b75.lua
@@ -0,0 +1,19 @@
+help([[
+This module loads GraphST (commit da29b75).
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD module for graphst/da29b75")
+    LmodMessage("GPU support with pytorch requires CUDA >= 11.3.")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD module for graphst/da29b75")
+end
+
+-- Directly activate or deactivate the conda environment
+execute {cmd="conda activate /jhpce/shared/jhpce/libd/graphst/da29b75/graphst_env", modeA={"load"}}
+execute {cmd="conda deactivate", modeA={"unload"}}
