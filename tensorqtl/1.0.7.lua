9514f26acb9b0b7236e00a479bd225d9312b412c
Nick-Eagles <nick.eagles@libd.org>
2023-03-22 16:52:52 -0400

    Add lua for tensorqtl 1.0.7. There are still rpy2 issues despite the fix that worked for 1.0.6

---
+++ tensorqtl/1.0.7.lua
@@ -0,0 +1,21 @@
+help([[
+This module loads tensorQTL 1.0.7 for python 3.10.6 packaged with conda_R/4.2.x.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD module for tensorQTL/1.0.7")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD module for tensorQTL/1.0.7")
+end
+
+load('conda_R/4.2.x')
+prepend_path("LD_LIBRARY_PATH", "/jhpce/shared/jhpce/core/conda/miniconda3-4.11.0/envs/svnR-4.2.x/R/4.2.x/lib64/R/lib")
+
+-- Directly activate or deactivate the python virtual environment
+execute {cmd="source /jhpce/shared/jhpce/libd/tensorqtl/1.0.7/tensorqtl_venv/bin/activate", modeA={"load"}}
+execute {cmd="deactivate", modeA={"unload"}}
