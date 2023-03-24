85c019a904040f902a0ba76dd88d5af0b0d48edf
Nick-Eagles <nick.eagles@libd.org>
2023-03-24 15:43:03 -0400

    Load conda_R/4.1.x instead of conda_R/4.2.x, which seems to fix an rpy2 issue

--- tensorqtl/1.0.7.lua
+++ tensorqtl/1.0.7.lua
@@ -1,5 +1,5 @@
 help([[
-This module loads tensorQTL 1.0.7 for python 3.10.6 packaged with conda_R/4.2.x.
+This module loads tensorQTL 1.0.7 for python 3.9.0 packaged with conda_R/4.1.x.
 ]])
 
 if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
@@ -13,8 +13,8 @@ elseif (mode() == "unload") then
     LmodMessage("Unloading LIBD module for tensorQTL/1.0.7")
 end
 
-load('conda_R/4.2.x')
-prepend_path("LD_LIBRARY_PATH", "/jhpce/shared/jhpce/core/conda/miniconda3-4.11.0/envs/svnR-4.2.x/R/4.2.x/lib64/R/lib")
+load('conda_R/4.1.x')
+prepend_path("LD_LIBRARY_PATH", "/jhpce/shared/jhpce/core/conda/miniconda3-4.6.14/envs/svnR-4.1.x/R/4.1.x/lib64/R/lib")
 
 -- Directly activate or deactivate the python virtual environment
 execute {cmd="source /jhpce/shared/jhpce/libd/tensorqtl/1.0.7/tensorqtl_venv/bin/activate", modeA={"load"}}
