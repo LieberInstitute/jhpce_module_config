b9dcd590ddc53f59c5804b76be19a9cc7e7ce3d6
Nick-Eagles <nick.eagles@libd.org>
2023-05-19 09:52:43 -0400

    Add lua for paste 1.3.0

---
+++ paste/1.3.0.lua
@@ -0,0 +1,20 @@
+help([[
+This module loads paste 1.3.0 for python 3.8.3.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD module for paste/1.3.0")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD module for paste/1.3.0")
+end
+
+load('python/3.8.3')
+
+-- Directly activate or deactivate the python virtual environment
+execute {cmd="source /jhpce/shared/jhpce/libd/paste/1.3.0/paste_venv/bin/activate", modeA={"load"}}
+execute {cmd="deactivate", modeA={"unload"}}
