6bbc9794bc089f63a7a607f4bca9ba3399973d04
Leonardo Collado Torres <lcolladotor@gmail.com>
2023-06-29 15:57:58 -0400

    Add config file for ruby 3.2.2 (not working yet!)

---
+++ ruby/3.2.2.lua
@@ -0,0 +1,18 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads ruby version 3.2.2.
+]])
+
+whatis("ruby")
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD module for ruby/3.2.2")
+prepend_path("PATH", "/jhpce/shared/libd/core/ruby/3.2.2/rubyinstall/bin")
+prepend_path("MANPATH","/jhpce/shared/libd/core/ruby/3.2.2/rubyinstall/share/man/man1")
+
