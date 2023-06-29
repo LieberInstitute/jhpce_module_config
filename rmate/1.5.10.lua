a421dd09a7e4868bd0a4e2aba623da9892348b1a
lcolladotor <lcolladotor@gmail.com>
2023-06-29 17:19:23 -0400

    Load Ruby 3.2.2 as a dependency for rmate 1.5.10

--- rmate/1.5.10.lua
+++ rmate/1.5.10.lua
@@ -7,11 +7,11 @@ This module loads rmate version 1.5.10.
 
 whatis("rmate")
 
-if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then
     LmodError("\
 This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
 end
 
 LmodMessage("Loading LIBD module for rmate/1.5.10")
-load('ruby/3.1.2')
+load('ruby/3.2.2')
 prepend_path("PATH", "/jhpce/shared/libd/core/rmate/1.5.10/rmate/bin")
