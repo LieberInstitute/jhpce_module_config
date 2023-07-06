8d6db6bb3020a0904fd24b3a1481419ac4206c93
Nick-Eagles <nick.eagles@libd.org>
2023-07-06 16:47:23 -0400

    Add lua for RSeQC/3.0.1. Only one PATH variable needs to be set, but we also need to activate the conda env

--- rseqc/3.0.1.lua
+++ rseqc/3.0.1.lua
@@ -7,12 +7,20 @@ This module loads RSeQC version 3.0.1.
 
 whatis("RSeQC")
 
-if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then
     LmodError("\
 This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
 end
 
-LmodMessage("Loading LIBD module for RSeQC/3.0.1")
-load('python/3.7.3')
-prepend_path("PYTHONPATH", "/jhpce/shared/libd/core/rseqc/3.0.1/RSeQC-3.0.1/bin//jhpce/shared/jhpce/core/python/3.7.3/lib/python3.7/site-packages")
-prepend_path("PATH", "/jhpce/shared/libd/core/rseqc/3.0.1/RSeQC-3.0.1/bin//jhpce/shared/jhpce/core/python/3.7.3/bin/")
+if (mode() == "load") then
+    LmodMessage("Loading LIBD module for RSeQC/3.0.1")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD module for RSeQC/3.0.1")
+end
+
+-- Directly activate or deactivate the conda environment
+execute {cmd="conda activate /jhpce/shared/libd/core/rseqc/3.0.1/rseqc_env", modeA={"load"}}
+execute {cmd="conda deactivate", modeA={"unload"}}
+
+-- This gives access to the physical python scripts
+prepend_path("PATH", "/jhpce/shared/libd/core/rseqc/3.0.1/RSeQC-3.0.1/scripts")
