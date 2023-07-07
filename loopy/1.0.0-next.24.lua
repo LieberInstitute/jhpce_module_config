dada532c830b29f11dc8f07c7e13b77a26cc47eb
Nick-Eagles <nick.eagles@libd.org>
2023-07-07 10:47:12 -0400

    Rename 'loopy' to 'samui'

--- loopy/1.0.0-next.24.lua
+++ loopy/1.0.0-next.24.lua
@@ -1,5 +1,5 @@
 help([[
-This module loads Loopy Browser v1.0.0-next.24.
+This module loads Samui Browser v1.0.0-next.24.
 ]])
 
 if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
@@ -8,14 +8,14 @@ This package can only be loaded on a compute or transfer node. Please use qrsh t
 end
 
 if (mode() == "load") then
-    LmodMessage("Loading LIBD module for loopy/1.0.0-next.24")
+    LmodMessage("Loading LIBD module for samui/1.0.0-next.24")
 elseif (mode() == "unload") then
-    LmodMessage("Unloading LIBD module for loopy/1.0.0-next.24")
+    LmodMessage("Unloading LIBD module for samui/1.0.0-next.24")
 end
 
 -- Directly activate or deactivate the conda environment
-execute {cmd="conda activate /jhpce/shared/libd/core/loopy/1.0.0-next.24/samui_env", modeA={"load"}}
+execute {cmd="conda activate /jhpce/shared/libd/core/samui/1.0.0-next.24/samui_env", modeA={"load"}}
 execute {cmd="conda deactivate", modeA={"unload"}}
 
 -- Seems to be necessary for finding python scripts from the Samui repo
-append_path('PYTHONPATH', '/jhpce/shared/libd/core/loopy/1.0.0-next.24/samui')
+append_path('PYTHONPATH', '/jhpce/shared/libd/core/samui/1.0.0-next.24/samui')
