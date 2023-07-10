77eb276cc583b6d8b9dafcef7b30d2329689ec2d
Nick-Eagles <nick.eagles@libd.org>
2023-07-10 11:12:22 -0400

    Rename 'featurecounts' to 'subread' (which matches the source repo and better describes the software); update lua for subread/2.0.0

--- subread/2.0.0.lua
+++ subread/2.0.0.lua
@@ -2,7 +2,7 @@
 -- vim:ft=lua:et:ts=4
 
 help([[
-This module loads featureCounts version 2.0.0.
+This module loads Subread version 2.0.0.
 ]])
 
 whatis("featureCounts")
@@ -12,5 +12,10 @@ if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.ge
 This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
 end
 
-LmodMessage("Loading LIBD module for featureCounts/2.0.0")
+if (mode() == "load") then
+    LmodMessage("Loading LIBD module for Subread/2.0.0")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD module for Subread/2.0.0")
+end
+
 prepend_path("PATH", "/jhpce/shared/libd/core/subread/2.0.0/subread-2.0.0-Linux-x86_64/bin")
