344b89882bb57af73006405bffcf8fbada5079d3
Nick-Eagles <nick.eagles@libd.org>
2023-07-14 10:25:38 -0400

    Add lua for wigToBigWig 2.9

---
+++ wigtobigwig/2.9.lua
@@ -0,0 +1,20 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads wigToBigWig (from UCSC tools) version 2.9.
+]])
+
+whatis("FastQC")
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD module for wigToBigWig/2.9")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD module for wigToBigWig/2.9")
+end
+prepend_path("PATH", "/jhpce/shared/libd/core/wigtobigwig/2.9")
