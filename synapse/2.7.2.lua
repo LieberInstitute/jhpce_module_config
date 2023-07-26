7a7c156f1bae327bb4f5331c661e1ffb63134385
Nick-Eagles <nick.eagles@libd.org>
2023-07-26 13:32:12 -0400

    Update lua for synapse 2.6.0 to 2.7.2

--- synapse/2.7.2.lua
+++ synapse/2.7.2.lua
@@ -1,5 +1,5 @@
 help([[
-This module loads synapse 2.6.0: namely, the command-line tool and python package.
+This module loads synapse 2.7.2: namely, the command-line tool and python package.
 ]])
 
 if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
@@ -8,11 +8,11 @@ This package can only be loaded on a compute or transfer node. Please use qrsh t
 end
 
 if (mode() == "load") then
-    LmodMessage("Loading LIBD module for synapse/2.6.0")
+    LmodMessage("Loading LIBD module for synapse/2.7.2")
 elseif (mode() == "unload") then
-    LmodMessage("Unloading LIBD module for synapse/2.6.0")
+    LmodMessage("Unloading LIBD module for synapse/2.7.2")
 end
 
 -- Directly activate or deactivate the conda environment
-execute {cmd="conda activate /jhpce/shared/jhpce/libd/synapse/2.6.0/synapse_env", modeA={"load"}}
+execute {cmd="conda activate /jhpce/shared/jhpce/libd/synapse/2.7.2/synapse_env", modeA={"load"}}
 execute {cmd="conda deactivate", modeA={"unload"}}
