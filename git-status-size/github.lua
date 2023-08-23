8b1ea67d0da178dbbb4116c5c546774f20869f1d
Nick-Eagles <nick.eagles@libd.org>
2023-08-23 10:29:09 -0400

    Fix how error handling is done when attempting to load a module on the login node, following a conversation with bithelp. Also improve the error message itself following advice from bithelp

--- git-status-size/github.lua
+++ git-status-size/github.lua
@@ -7,9 +7,11 @@ This module loads git-status-size version github.
 
 whatis("git-status-size")
 
-if not string.match(os.getenv("SLURMD_NODENAME"),"compute") and not  string.match(os.getenv("SLURMD_NODENAME"),"transfer") then  
-    LmodError("\
-This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+-- Check if the env var SLURMD_NODENAME exists to indirectly verify if a user
+-- is on a compute or transfer node
+node_name = os.getenv("SLURMD_NODENAME") or "undefined"
+if (node_name == "undefined") then
+    LmodError("This package can only be loaded on a compute or transfer node. Please use a SLURM command (srun, sbatch, salloc) to a compute or transfer node.")
 end
 
 LmodMessage("Loading LIBD module for git-status-size/github")
--- samblaster/0.1.26.lua
+++ samblaster/0.1.26.lua
@@ -5,9 +5,11 @@ help([[
 This module loads samblaster v0.1.26.
 ]])
 
-if not string.match(os.getenv("SLURMD_NODENAME"),"compute") and not string.match(os.getenv("SLURMD_NODENAME"),"transfer") then  
-    LmodError("\
-This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+-- Check if the env var SLURMD_NODENAME exists to indirectly verify if a user
+-- is on a compute or transfer node
+node_name = os.getenv("SLURMD_NODENAME") or "undefined"
+if (node_name == "undefined") then
+    LmodError("This package can only be loaded on a compute or transfer node. Please use a SLURM command (srun, sbatch, salloc) to a compute or transfer node.")
 end
 
 LmodMessage("Loading LIBD module for samblaster/0.1.26")
