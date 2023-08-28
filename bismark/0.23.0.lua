908fa80bdf6f48fdae25bc9929180560c8db4d8a
Nick-Eagles <nick.eagles@libd.org>
2023-08-28 12:29:06 -0400

    Add lua for bismark 0.23.0

--- bismark/0.23.0.lua
+++ bismark/0.23.0.lua
@@ -1,9 +1,7 @@
 -- -*- lua -*-
 -- vim:ft=lua:et:ts=4
 
-help([[
-This module loads bismark 0.23.0.
-]])
+help([[This module loads bismark 0.23.0.]])
 
 -- Check if the env var SLURMD_NODENAME exists to indirectly verify if a user
 -- is on a compute or transfer node
@@ -12,5 +10,10 @@ if (node_name == "undefined") then
     LmodError("This package can only be loaded on a compute or transfer node. Please use a SLURM command (srun, sbatch, salloc) to a compute or transfer node.")
 end
 
-LmodMessage("Loading LIBD module for bismark/0.23.0")
+if (mode() == "load") then
+    LmodMessage("Loading LIBD module for bismark/0.23.0")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD module for bismark/0.23.0")
+end
+
 prepend_path("PATH", "/jhpce/shared/libd/core/bismark/0.23.0/Bismark-0.23.0")
