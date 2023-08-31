7456819e56e470c9fda600b78f9579ce59734f5c
Nick-Eagles <nick.eagles@libd.org>
2023-08-31 12:01:28 -0400

    Add lua for tangram 1.0.4 and remove the unused 1.0.2

---
+++ tangram/1.0.4.lua
@@ -0,0 +1,22 @@
+help([[This module loads tangram 1.0.4 for python 3.8.5.]])
+
+-- Check if the env var SLURMD_NODENAME exists to indirectly verify if a user
+-- is on a compute or transfer node
+node_name = os.getenv("SLURMD_NODENAME") or "undefined"
+if (node_name == "undefined") then
+    LmodError("This package can only be loaded on a compute or transfer node. Please use a SLURM command (srun, sbatch, salloc) to a compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD module for tangram/1.0.4")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD module for tangram/1.0.4")
+end
+
+-- Directly activate or deactivate the conda environment. Here we directly
+-- source conda-related code to circumvent the need for 'conda init', which is
+-- user dependent
+execute {cmd="source /jhpce/shared/jhpce/core/conda/miniconda3-23.3.1/etc/profile.d/conda.sh", modeA={"load"}}
+execute {cmd="conda deactivate", modeA={"load"}}
+execute {cmd="conda activate /jhpce/shared/libd/core/tangram/1.0.4/tangram_env", modeA={"load"}}
+execute {cmd="conda deactivate", modeA={"unload"}}
