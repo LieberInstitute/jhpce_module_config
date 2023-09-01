a19d66360453162d35e22c414aebaa5fc52bc82e
Nick-Eagles <nick.eagles@libd.org>
2023-09-01 16:12:34 -0400

    Add lua for plink 2.00 alpha 4.6

---
+++ plink/2.00a4.6.lua
@@ -0,0 +1,22 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[This module loads plink version 2.00 Alpha 4.6.]])
+
+whatis("plink")
+
+-- Check if the env var SLURMD_NODENAME exists to indirectly verify if a user
+-- is on a compute or transfer node
+node_name = os.getenv("SLURMD_NODENAME") or "undefined"
+if (node_name == "undefined") then
+    LmodError("This package can only be loaded on a compute or transfer node. Please use a SLURM command (srun, sbatch, salloc) to a compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD module for plink/2.00a4.6")
+    LmodMessage("Plink is invoked with the command 'plink2'.")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD module for plink/2.00a4.6")
+end
+
+prepend_path("PATH", "/jhpce/shared/libd/core/plink/2.00a4.6")
