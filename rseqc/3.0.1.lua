41dfe5c89ad23b3e3f88e3712acc18ecb88668f1
Nick-Eagles <nick.eagles@libd.org>
2023-09-21 17:02:06 -0400

    Directly source the conda init command to prevent reliance on user-specific configuration

--- rseqc/3.0.1.lua
+++ rseqc/3.0.1.lua
@@ -20,7 +20,10 @@ elseif (mode() == "unload") then
     LmodMessage("Unloading LIBD module for RSeQC/3.0.1")
 end
 
--- Directly activate or deactivate the conda environment
+-- Directly activate or deactivate the conda environment. Here we directly
+-- source conda-related code to circumvent the need for 'conda init', which is
+-- user dependent
+execute {cmd="source /jhpce/shared/jhpce/core/conda/miniconda3-23.3.1/etc/profile.d/conda.sh", modeA={"load"}}
 execute {cmd="conda activate /jhpce/shared/libd/core/rseqc/3.0.1/rseqc_env", modeA={"load"}}
 execute {cmd="conda deactivate", modeA={"unload"}}
 
