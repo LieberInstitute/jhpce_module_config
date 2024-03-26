98de099bf40e345b18067a1df7cb715b3a8f5640
Nick-Eagles <nick.eagles@libd.org>
2024-03-26 13:38:19 -0400

    Add lua for liftOver

---
+++ liftover/1.0.lua
@@ -0,0 +1,21 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads liftOver.
+]])
+
+-- Check if the env var SLURMD_NODENAME exists to indirectly verify if a user
+-- is on a compute or transfer node
+node_name = os.getenv("SLURMD_NODENAME") or "undefined"
+if (node_name == "undefined") then
+    LmodError("This package can only be loaded on a compute or transfer node. Please use a SLURM command (srun, sbatch, salloc) to a compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD SLURM module for liftover/1.0")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD SLURM module for liftover/1.0")
+end
+
+prepend_path("PATH", "/jhpce/shared/libd/core/liftover/1.0")
