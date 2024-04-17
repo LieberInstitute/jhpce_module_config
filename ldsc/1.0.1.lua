6aae08c117f813eae8b85a00ea02fe4090cba95f
Nick-Eagles <nick.eagles@libd.org>
2024-04-17 15:10:45 -0400

    Remake ldsc 1.0.1 lua to correctly load the conda environment and have better printed messages

--- ldsc/1.0.1.lua
+++ ldsc/1.0.1.lua
@@ -2,7 +2,7 @@
 -- vim:ft=lua:et:ts=4
 
 help([[
-This module loads LDSC version 1.0.1.
+This module loads bulik/ldsc version 1.0.1.
 ]])
 
 if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
@@ -10,7 +10,19 @@ if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.ge
 This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
 end
 
-LmodMessage("Loading LIBD SLURM module for LDSC/1.0.1")
-load('python/3.7.3')
+if (mode() == "load") then
+    LmodMessage("Loading LIBD SLURM module for ldsc/1.0.1")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD SLURM module for ldsc/1.0.1")
+end
+
+-- To make it easier to directly invoke python scripts in the repo
 prepend_path("PATH", "/jhpce/shared/libd/core/ldsc/1.0.1/ldsc")
-setenv('OPENBLAS_NUM_THREADS', 1)
+
+-- Directly activate or deactivate the conda environment
+always_load("conda/3-23.3.1")
+execute {cmd="conda activate /jhpce/shared/libd/core/ldsc/1.0.1/ldsc_env", modeA={"load"}}
+execute {cmd="conda deactivate", modeA={"unload"}}
+
+-- Don't try to depend on user-installed python packages
+setenv("PYTHONNOUSERSITE", "anything")
