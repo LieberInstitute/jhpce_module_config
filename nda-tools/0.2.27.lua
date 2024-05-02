e2ceda46a8980c3f3749f4393fd487208f32a12b
Nick-Eagles <nick.eagles@libd.org>
2024-05-02 11:49:59 -0400

    Add lua for nda-tools 0.2.27

---
+++ nda-tools/0.2.27.lua
@@ -0,0 +1,22 @@
+help([[
+This module loads nda-tools, the NIMH Data Archive python-based client.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD SLURM module for nda-tools/0.2.27")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD SLURM module for nda-tools/0.2.27")
+end
+
+-- Directly activate or deactivate the conda environment
+always_load("conda/3-23.3.1")
+execute {cmd="conda activate /jhpce/shared/libd/core/nda-tools/0.2.27/nda_env", modeA={"load"}}
+execute {cmd="conda deactivate", modeA={"unload"}}
+
+-- Don't try to depend on user-installed python packages
+setenv("PYTHONNOUSERSITE", "anything")
