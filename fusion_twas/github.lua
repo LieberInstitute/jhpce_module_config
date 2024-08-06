82d528570bb9844528620dee6793f6248f4a4dbe
Nick-Eagles <nick.eagles@libd.org>
2024-08-06 11:49:45 -0400

    Update the fusion_twas lua to use the correct plink, more informative messages, and refer to the updated installation

--- fusion_twas/github.lua
+++ fusion_twas/github.lua
@@ -2,19 +2,21 @@
 -- vim:ft=lua:et:ts=4
 
 help([[
-This module loads fusion_twas version github (c677be0).
+This module loads fusion_twas (GitHub commit e1ba5f7).
 ]])
-
-whatis("HISAT2")
 
 if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
     LmodError("\
-This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
+This package can only be loaded on a compute or transfer node. Please use srun to connect to a valid compute or transfer node.")
 end
 
-LmodMessage("Loading LIBD SLURM module for fusion_twas/github")
-load('plink/1.90b6.6')
-prepend_path("PATH", "/jhpce/shared/libd/core/fusion_twas/github/fusion_twas")
+if (mode() == "load") then
+    LmodMessage("Loading LIBD SLURM module for fusion_twas/github")
+    LmodMessage("This module does not come with R dependencies; check http://gusevlab.org/projects/fusion/#installation to install any required packages for your user.")
+    LmodMessage("Invoke R scripts as in this example: 'Rscript $(which FUSION.compute_weights.R) --help'")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD SLURM module for fusion_twas/github")
+end
 
-LmodMessage("See /jhpce/shared/libd/core/fusion_twas/github/README.md for installing the R dependencies")
-LmodMessage("Run using the full path, for example: Rscript /jhpce/shared/libd/core/fusion_twas/github/fusion_twas/FUSION.compute_weights.R --help")
+load('plink/1.90b')
+prepend_path("PATH", "/jhpce/shared/libd/core/fusion_twas/github/fusion_twas-master")
