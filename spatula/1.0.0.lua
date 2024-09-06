ba5edc27ce32bb3d16f72eaa044de715233af855
Geo Pertea <geo.pertea@gmail.com>
2024-09-06 09:42:28 -0400

    drop unnecessary remove_path() call, Lmod does it automatically

--- spatula/1.0.0.lua
+++ spatula/1.0.0.lua
@@ -12,13 +12,10 @@ if not string.match(os.getenv("HOSTNAME"),"compute") then
 This module can only be loaded on a compute node. Please use an interactive session or submit a job to use it.")
 end
 
-local bin_path = "/jhpce/shared/libd/core/spatula/1.0.0"
-
 if (mode() == "load") then
     LmodMessage("Loading LIBD SLURM module for spatula/1.0.0")
-    prepend_path("PATH", bin_path)
 elseif (mode() == "unload") then
     LmodMessage("Unloading LIBD SLURM module for spatula/1.0.0")
-    remove_path("PATH", bin_path)
 end
 
+prepend_path("PATH", "/jhpce/shared/libd/core/spatula/1.0.0")
