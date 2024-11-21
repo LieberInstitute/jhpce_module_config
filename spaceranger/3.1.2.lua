604b7e263d003665928f6e239dcb8fe1a0980ea7
Nick-Eagles <nick.eagles@libd.org>
2024-11-21 12:18:34 -0500

    Add lua for recent versions of spaceranger

---
+++ spaceranger/3.1.2.lua
@@ -0,0 +1,20 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads space ranger version 3.1.2
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use srun to connect to a valid compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD SLURM module for spaceranger/3.1.2")
+    LmodMessage("Reference files, for use with the '--transcriptome' argument, can be accessed or downloaded into /dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x.")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD SLURM module for spaceranger/3.1.2")
+end
+
+prepend_path("PATH", "/jhpce/shared/libd/core/spaceranger/3.1.2/spaceranger-3.1.2/bin")
