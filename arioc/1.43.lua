7ae9df638861ee92b611cca4f4f70fd23ae5a83f
Nick-Eagles <nick.eagles@libd.org>
2025-04-23 15:17:13 -0400

    Add lua for Arioc 1.43

---
+++ arioc/1.43.lua
@@ -0,0 +1,17 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[This module loads Arioc version 1.43.]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use srun to connect to a valid compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD SLURM module for Arioc/1.43")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD SLURM module for Arioc/1.43")
+end
+
+prepend_path("PATH", "/jhpce/shared/libd/core/arioc/1.43/bin")
