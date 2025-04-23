2b09b49a76beee1b538ea86fc3719d263b4c970c
Nick-Eagles <nick.eagles@libd.org>
2025-04-23 14:23:03 -0400

    The system default java seems to be necessary with this nextflow version

--- nextflow/20.01.0.lua
+++ nextflow/20.01.0.lua
@@ -10,8 +10,6 @@ if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.ge
 This package can only be loaded on a compute or transfer node. Please use qrsh to connect to a valid compute or transfer node.")
 end
 
-load('java/18')
-
 if (mode() == "load") then
     LmodMessage("Loading LIBD SLURM module for nextflow/20.01.0")
     LmodMessage('To instruct Nextflow to allocate enough memory for Java, consider running something like: export NXF_JVM_ARGS="-Xms5g -Xmx6g".')
