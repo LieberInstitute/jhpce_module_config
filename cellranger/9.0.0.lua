1f06fcb1d73a0df878c2aac5565e49ed947005b1
Ryan Miller <ry.a.miller3@gmail.com>
2025-05-27 12:59:29 -0400

    new lua files for cellranger versions 9.0.0 and 9.0.1

---
+++ cellranger/9.0.0.lua
@@ -0,0 +1,15 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads cell ranger version 9.0.0.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use srun to connect to a valid compute or transfer node.")
+end
+
+LmodMessage("Loading LIBD SLURM module for cellranger/9.0.0")
+LmodMessage("Reference files, for use with the '--transcriptome' argument, can be accessed or downloaded into /dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x.")
+prepend_path("PATH", "/jhpce/shared/libd/core/cellranger/9.0.0/cellranger-9.0.0/bin")
