c53aafe21161f7e6ad4b189d3c55cddb023d3084
Nick-Eagles <nick.eagles@libd.org>
2026-05-19 10:36:15 -0400

    Add lua for rust/1.95.0

---
+++ rust/1.95.0.lua
@@ -0,0 +1,17 @@
+help([[
+This module loads binaries associated with Rust 1.95.0.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use srun to connect to a valid compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD SLURM module for rust/1.95.0")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD SLURM module for rust/1.95.0")
+end
+
+prepend_path("PATH", "/jhpce/shared/libd/core/rust/1.95.0/rust-1.95.0-x86_64-unknown-linux-gnu/bin")
+prepend_path("LD_LIBRARY_PATH", "/jhpce/shared/libd/core/rust/1.95.0/rust-1.95.0-x86_64-unknown-linux-gnu/lib")
