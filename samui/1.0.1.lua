948fa4d020188fb8d68959a9731bb06eb514a8fd
Ryan Miller <ry.a.miller3@gmail.com>
2026-06-23 11:25:25 -0400

    updated lua files for new versions of spaceranger, xeniumranger, nda-tools, and samui

---
+++ samui/1.0.1.lua
@@ -0,0 +1,25 @@
+help([[
+This module loads Samui Browser v1.0.1.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use srun to connect to a valid compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD SLURM module for samui/1.0.1")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD SLURM module for samui/1.0.1")
+end
+
+-- Directly activate or deactivate the conda environment
+always_load("conda/3-24.3.0")
+execute {cmd="conda activate /jhpce/shared/libd/core/samui/1.0.1/samui_env", modeA={"load"}}
+execute {cmd="conda deactivate", modeA={"unload"}}
+
+-- Seems to be necessary for finding python scripts from the Samui repo
+append_path('PYTHONPATH', '/jhpce/shared/libd/core/samui/1.0.1/samui')
+
+-- Don't try to depend on user-installed python packages
+setenv("PYTHONNOUSERSITE", "anything")
---
+++ xeniumranger/4.0.0.lua
@@ -0,0 +1,20 @@
+-- -*- lua -*-
+-- vim:ft=lua:et:ts=4
+
+help([[
+This module loads Xenium Ranger version 4.0.0.
+]])
+
+if not string.match(os.getenv("HOSTNAME"),"compute") and not  string.match(os.getenv("HOSTNAME"),"transfer") then  
+    LmodError("\
+This package can only be loaded on a compute or transfer node. Please use srun to connect to a valid compute or transfer node.")
+end
+
+if (mode() == "load") then
+    LmodMessage("Loading LIBD SLURM module for xeniumranger/4.0.0")
+    LmodMessage("Reference files, for use with the '--transcriptome' argument, can be accessed or downloaded into /dcs04/lieber/lcolladotor/annotationFiles_LIBD001/10x.")
+elseif (mode() == "unload") then
+    LmodMessage("Unloading LIBD SLURM module for xeniumranger/4.0.0")
+end
+
+prepend_path("PATH", "/jhpce/shared/libd/core/xeniumranger/4.0.0/xeniumranger-4.0.0/bin")
